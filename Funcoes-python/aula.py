f3d59179967c8c6bece65c8284adb28d351324c0
Gabriel Magalhães de Oliveira <99506666+MagalGabriel@users.noreply.github.com>
2024-08-26 09:43:20 -0300

    Add files via upload

---
+++ Funcoes-python/aula.py
@@ -0,0 +1,19 @@
+print("Teste de python aula 1 - Mineração de dados")
+
+n1 = 30
+n2 = 20
+
+adicao = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+
+
+print("Adição: ", adicao)
+if subtracao < 0:
+    print("Subtração é menor que 0")
+else:
+    print("Subtração: ", subtracao)
+
+print("Multiplicação: ", multiplicacao)
+print("Divisão: ", divisao)
